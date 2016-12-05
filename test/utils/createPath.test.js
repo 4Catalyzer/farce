aa0a6a3b856ba1c068d62186143868c8d20312e7
C.T. Lin <chentsulin@gmail.com>
2016-12-05 10:08:01 +0800

    Added tests for utils/createPath.js

---
+++ test/utils/createPath.test.js
@@ -0,0 +1,11 @@
+import createPath from '../../src/utils/createPath';
+
+describe('createPath', () => {
+  it('should create path using pathname, search, and hash', () => {
+    expect(createPath({
+      pathname: '/foo',
+      search: '?bar=baz',
+      hash: '#qux',
+    })).to.equal('/foo?bar=baz#qux');
+  });
+});
