f3fd3946f917c05cfe3538e69ee88c1081aaed47
C.T. Lin <chentsulin@gmail.com>
2016-12-05 09:47:42 +0800

    Added test for top level export

---
+++ test/index.test.js
@@ -0,0 +1,22 @@
+import * as farce from '../src';
+
+describe('index', () => {
+  it('should export top level correctly', () => {
+    expect(farce.Actions).to.exist();
+    expect(farce.ActionTypes).to.exist();
+    expect(farce.BrowserProtocol).to.exist();
+    expect(farce.createBasenameMiddleware).to.exist();
+    expect(farce.createHistory).to.exist();
+    expect(farce.createHistoryEnhancer).to.exist();
+    expect(farce.createHistoryMiddleware).to.exist();
+    expect(farce.createLocationMiddleware).to.exist();
+    expect(farce.createQueryMiddleware).to.exist();
+    expect(farce.createStoreHistory).to.exist();
+    expect(farce.createTransitionHookMiddleware).to.exist();
+    expect(farce.ensureLocationMiddleware).to.exist();
+    expect(farce.locationReducer).to.exist();
+    expect(farce.queryMiddleware).to.exist();
+    expect(farce.ServerProtocol).to.exist();
+    expect(farce.StateStorage).to.exist();
+  });
+});
