65ac838a934dab51b275f839421076c4162b106e
C.T. Lin <chentsulin@gmail.com>
2016-12-05 23:50:22 +0800

    Added tests for ensureLocationMiddleware.js

---
+++ test/ensureLocationMiddleware.test.js
@@ -0,0 +1,92 @@
+import sinon from 'sinon';
+
+import ActionTypes from '../src/ActionTypes';
+import ensureLocationMiddleware from '../src/ensureLocationMiddleware';
+
+describe('ensureLocationMiddleware', () => {
+  let next;
+  let dispatch;
+  beforeEach(() => {
+    next = sinon.spy();
+    dispatch = ensureLocationMiddleware()(next);
+  });
+
+  it('should ensure location of PUSH action', () => {
+    dispatch({
+      type: ActionTypes.PUSH,
+      payload: '/foo?bar=baz#qux',
+    });
+
+    expect(next).to.be.calledWith({
+      type: ActionTypes.TRANSITION,
+      payload: {
+        action: 'PUSH',
+        pathname: '/foo',
+        search: '?bar=baz',
+        hash: '#qux',
+      },
+    });
+  });
+
+  it('should ensure location of REPLACE action', () => {
+    dispatch({
+      type: ActionTypes.REPLACE,
+      payload: '/foo?bar=baz#qux',
+    });
+
+    expect(next).to.be.calledWith({
+      type: ActionTypes.TRANSITION,
+      payload: {
+        action: 'REPLACE',
+        pathname: '/foo',
+        search: '?bar=baz',
+        hash: '#qux',
+      },
+    });
+  });
+
+  it('should ensure location of CREATE_HREF action', () => {
+    dispatch({
+      type: ActionTypes.CREATE_HREF,
+      payload: '/foo?bar=baz#qux',
+    });
+
+    expect(next).to.be.calledWith({
+      type: ActionTypes.CREATE_HREF,
+      payload: {
+        pathname: '/foo',
+        search: '?bar=baz',
+        hash: '#qux',
+      },
+    });
+  });
+
+  it('should ensure location of CREATE_LOCATION action', () => {
+    dispatch({
+      type: ActionTypes.CREATE_LOCATION,
+      payload: '/foo?bar=baz#qux',
+    });
+
+    expect(next).to.be.calledWith({
+      type: ActionTypes.CREATE_LOCATION,
+      payload: {
+        pathname: '/foo',
+        search: '?bar=baz',
+        hash: '#qux',
+      },
+    });
+  });
+
+  it('should not affect other action', () => {
+    const UNKNOWN = 'UNKNOWN';
+    dispatch({
+      type: UNKNOWN,
+      payload: '/foo?bar=baz#qux',
+    });
+
+    expect(next).to.be.calledWith({
+      type: UNKNOWN,
+      payload: '/foo?bar=baz#qux',
+    });
+  });
+});
