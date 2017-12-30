d8cd171fefff0b47333950a65ae6656631add6bd
Jimmy Jia <tesrin@gmail.com>
2017-12-30 15:18:45 -0800

    Improve testing

---
+++ test/createHistoryEnhancer.test.js
@@ -0,0 +1,86 @@
+import { createStore } from 'redux';
+
+import Actions from '../src/Actions';
+import createHistoryEnhancer from '../src/createHistoryEnhancer';
+import locationReducer from '../src/locationReducer';
+import MemoryProtocol from '../src/MemoryProtocol';
+
+describe('createHistoryMiddleware', () => {
+  let store;
+
+  beforeEach(() => {
+    store = createStore(
+      locationReducer,
+      createHistoryEnhancer({ protocol: new MemoryProtocol('/foo') }),
+    );
+    store.dispatch(Actions.init());
+  });
+
+  afterEach(() => {
+    store.dispatch(Actions.dispose());
+  });
+
+  it('should support pushing and popping', () => {
+    store.dispatch(Actions.push('/bar'));
+    expect(store.getState()).to.include({
+      pathname: '/bar',
+      index: 1,
+    });
+
+    store.dispatch(Actions.go(-1));
+    expect(store.getState()).to.include({
+      pathname: '/foo',
+      index: 0,
+    });
+
+    store.dispatch(Actions.go(+1));
+    expect(store.getState()).to.include({
+      pathname: '/bar',
+      index: 1,
+    });
+  });
+
+  it('should support replacing', () => {
+    store.dispatch(Actions.replace('/bar'));
+    expect(store.getState()).to.include({
+      pathname: '/bar',
+      index: 0,
+    });
+  });
+
+  it('should create href', () => {
+    expect(
+      store.farce.createHref({
+        pathname: '/foo',
+        search: '?bar',
+        hash: '#baz',
+      }),
+    ).to.equal('/foo?bar#baz');
+  });
+
+  it('should create location', () => {
+    expect(
+      store.farce.createLocation({
+        pathname: '/foo',
+        search: '?bar',
+        hash: '#baz',
+      }),
+    ).to.eql({
+      pathname: '/foo',
+      search: '?bar',
+      hash: '#baz',
+    });
+  });
+
+  it('should ignore other actions', () => {
+    expect(
+      store.dispatch({
+        type: 'UNKNOWN',
+        payload: { unknown: {} },
+      }),
+    ).to.eql({
+      type: 'UNKNOWN',
+      payload: { unknown: {} },
+    });
+  });
+});
