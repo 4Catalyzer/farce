776e00b16a34f89d990a211deb138a1ea646a055
Jimmy Jia <tesrin@gmail.com>
2017-12-30 15:54:32 -0800

    Fix and test history integration

---
+++ test/createStoreHistory.test.js
@@ -0,0 +1,57 @@
+import { combineReducers, createStore } from 'redux';
+
+import Actions from '../src/Actions';
+import createHistoryEnhancer from '../src/createHistoryEnhancer';
+import createStoreHistory from '../src/createStoreHistory';
+import locationReducer from '../src/locationReducer';
+import MemoryProtocol from '../src/MemoryProtocol';
+
+describe('createStoreHistory', () => {
+  let store;
+  let history;
+  let listener;
+  let unlisten;
+
+  beforeEach(() => {
+    store = createStore(
+      combineReducers({
+        location: locationReducer,
+      }),
+      createHistoryEnhancer({
+        protocol: new MemoryProtocol('/foo'),
+      }),
+    );
+
+    store.dispatch(Actions.init());
+
+    history = createStoreHistory(store);
+
+    listener = sinon.spy();
+    unlisten = history.listen(listener);
+
+    listener.reset();
+  });
+
+  afterEach(() => {
+    unlisten();
+    history.dispose();
+  });
+
+  it('should only call listener on location updates', () => {
+    history.push('/bar');
+
+    expect(listener).to.have.been.calledOnce();
+    expect(listener.firstCall.args[0]).to.include({
+      pathname: '/bar',
+      index: 1,
+    });
+    listener.reset();
+
+    store.dispatch({
+      type: 'UNKNOWN',
+      payload: null,
+    });
+
+    expect(listener).not.to.have.been.called();
+  });
+});
