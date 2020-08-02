9edb4289689a357b9a37a112e8d30479c1d8957b
Jimmy Jia <tesrin@gmail.com>
2020-08-02 15:26:37 -0400

    feat: Improve Redux-related types (#351)

--- types/Actions.d.ts
+++ types/Actions.d.ts
@@ -1,3 +1,11 @@
 // TypeScript Version: 3.0
 
-export { Actions as default } from 'farce';
+export {
+  Actions as default,
+  Action,
+  DisposeAction,
+  GoAction,
+  InitAction,
+  PushAction,
+  ReplaceAction,
+} from 'farce';
